a17f13291464542a744b7885a6de7afdf832539a
Siegfred Samson <siegfredsamson@gmail.com>
2021-05-01 21:54:57 +0100

    api_class/urls.py updated

--- api_class/urls.py
+++ api_class/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api_class/', include("api/urls"))
+    path('api/', include("api.urls"))
 ]
