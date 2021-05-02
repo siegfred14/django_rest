cb2904e1991f61de5ec738ce61eeaa1c376e7898
Siegfred Samson <siegfredsamson@gmail.com>
2021-05-02 17:22:17 +0100

    urls.py path updated

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path('', home, name="api")
+    path('create-student/', views.StudentViews.as_view(), name="student")
 
 ]
