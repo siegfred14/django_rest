55c53a8c14f2f1aaac85131dc2ff7c73e472fdd0
Siegfred Samson <siegfredsamson@gmail.com>
2021-05-03 12:22:45 +0100

    changes made to views.py and urls.py for StudentViews

--- api/views.py
+++ api/views.py
@@ -29,24 +29,24 @@ from .serializers import StudentSerializer
 
 
 # making a post request
-# class StudentViews(generics.CreateAPIView):
-#     queryset = Student.objects.all()
-#     serializer_class = StudentSerializer
-#
-#     def create(self, request, *args, **kwargs):
-#         serializer = self.serializer_class(data=request.data)
-#
-#     # def perform_create(self, serializer):
-#     #     return super().perform_create(serializer)
+class StudentViews(generics.CreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
-@api_view(['POST'])
-def create_student(request):
-    student = Student.objects.all()
-    serializer = StudentSerializer(data=request.data)
+    # def create(self, request, *args, **kwargs):
+    #     serializer = self.serializer_class(data=request.data)
 
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+    # def perform_create(self, serializer):
+    #     return super().perform_create(serializer)
+
+# @api_view(['POST'])
+# def create_student(request):
+#     student = Student.objects.all()
+#     serializer = StudentSerializer(data=request.data)
+#
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
 
 
 class GetStudentViews(generics.ListAPIView):
