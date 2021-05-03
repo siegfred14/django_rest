8e33777aef5fec11dab23bbce7cde55ad57a26da
Siegfred Samson <siegfredsamson@gmail.com>
2021-05-03 10:24:06 +0100

    student properties declared in StudentSerializer in serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -4,6 +4,10 @@ from .models import Student
 
 class StudentSerializer(serializers.Serializer):
 
+    name = serializers.CharField(max_length=255)
+    email = serializers.EmailField()
+    reg_no = serializers.CharField()
+
     class Meta:
         model = Student
         fields = "__all__"
