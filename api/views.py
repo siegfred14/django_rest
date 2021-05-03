67675997809fde606cfa19b6a98c8b143db6b2bb
Siegfred Samson <siegfredsamson@gmail.com>
2021-05-03 11:35:25 +0100

    views.py changed to use api views

--- api/views.py
+++ api/views.py
@@ -20,6 +20,7 @@
  # - DELETE Deleting
 
 from rest_framework import generics
+from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
 
@@ -28,12 +29,24 @@ from .serializers import StudentSerializer
 
 
 # making a post request
-class StudentViews(generics.CreateAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
+# class StudentViews(generics.CreateAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+#
+#     def create(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(data=request.data)
+#
+#     # def perform_create(self, serializer):
+#     #     return super().perform_create(serializer)
 
-    # def perform_create(self, serializer):
-    #     return super().perform_create(serializer)
+@api_view
+def create_student(request):
+    student = Student.objects.all()
+    serializer = StudentSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
 
 
 class GetStudentViews(generics.ListAPIView):
