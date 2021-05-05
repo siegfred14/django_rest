fd677ec0af805f1dd5e744e1c3a0670e273a4437
Siegfred Samson <siegfredsamson@gmail.com>
2021-05-05 13:09:58 +0100

    accounts app urls.py created and registered in project urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path('create-student/', views.create_student, name="student"),
-    path('create-student/', views.StudentViews.as_view(), name="student"),
-    path('get-student/', views.GetStudentViews.as_view(), name="get-student"),
-    path('update-student/<int:pk>', views.UpdateStudentViews.as_view(), name="update-student"),
-    path('delete-student/<int:pk>', views.DeleteStudentViews.as_view(), name="delete-student")
+
 ]
