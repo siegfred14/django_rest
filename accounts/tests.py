536fbaaaf57cda286bc4b870385838b8fad0410a
Siegfred Samson <siegfredsamson@gmail.com>
2021-05-05 16:12:17 +0100

    test modified for user with short password and user with no password

--- accounts/tests.py
+++ accounts/tests.py
@@ -29,3 +29,31 @@ class UserTest(APITestCase):
         self.assertEqual(response.data['email'], data['email'])
         self.assertEqual(response.data['username'], data['username'])
         self.assertFalse('password' in response.data)
+
+    def test_create_user_with_short_password(self):
+        data = {
+            'username': 'foobar',
+            'email': 'foobar@example.com',
+            'password': 'somepassword'
+        }
+
+        response = self.client.post(self.create_url, data, format='json')
+
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data['password']), 1)
+        self.assertFalse('password' in response.data)
+
+    def test_create_user_with_no_password(self):
+        data = {
+            'username': 'foobar',
+            'email': 'foobar@example.com',
+            'password': ''
+        }
+
+        response = self.client.post(self.create_url, data, format='json')
+
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data['password']), 1)
+        self.assertFalse('password' in response.data)
