dc705278128078ad790533afe9be4ae8232e0e8a
Siegfred Samson <siegfredsamson@gmail.com>
2021-05-05 14:27:46 +0100

    views.py modified with class UserCreate

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+class UserCreate(APIView):
+    def post(self, request, format='json'):
+        return Response("hello")
